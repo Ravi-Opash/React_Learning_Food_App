b2ccc06647488c07a67fdb5e426a0d911b59704e
Ravi <rg.opash@gmail.com>
2023-03-09 15:44:00 +0530

    created 20% Cart

--- src/Components/UI/Model.js
+++ src/Components/UI/Model.js
@@ -1,26 +1,26 @@
-import React, { Fragment } from "react";
-import style from "./Model.module.css";
-import ReactDOM from "react-dom";
+// import React, { Fragment } from "react";
+// import style from "./Model.module.css";
+// import ReactDOM from "react-dom";
 
-const Backdrop = (props) => {
-  return <div className={style.backdrop}></div>;
-};
+// const Backdrop = (props) => {
+//   return <div className={style.backdrop}></div>;
+// };
 
-const ModelOverlay = (props) => {
-  return (
-    <div className={style.model}>
-      <div className={style.content}>{props.childern}</div>
-    </div>
-  );
-};
+// const ModelOverlay = (props) => {
+//   return (
+//     <div className={style.model}>
+//       <div className={style.content}>{props.childern}</div>
+//     </div>
+//   );
+// };
 
-const x = document.getElementById('overlays');
+// const x = document.getElementById('overlays');
 
-const Modal = (props) => {
-  <Fragment>
-    {ReactDOM.createPortal(<Backdrop />, x)}
-    {ReactDOM.createPortal(<ModelOverlay>{props.childern}</ModelOverlay>, x)}
-  </Fragment>;
-};
+// const Modal = (props) => {
+//   <Fragment>
+//     {ReactDOM.createPortal(<Backdrop />, x)}
+//     {ReactDOM.createPortal(<ModelOverlay>{props.childern}</ModelOverlay>, x)}
+//   </Fragment>;
+// };
 
-export default Modal;
+// export default Modal;
